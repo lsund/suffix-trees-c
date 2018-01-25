7b28e3137ef18bff9cd609644b4f23d26ff264e3
Ludvig Sundström <lud.sund@gmail.com>
2018-01-25 18:14:45 +0100

    Add module for vertex and implement basic functions

---
+++ include/vertex.h
@@ -0,0 +1,22 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include "label.h"
+
+typedef struct vertex {
+    int i;
+    struct vertex *c;
+    struct vertex *s;
+    struct vertex *p;
+    int k;
+} *Vertex;
+
+Vertex vertex(const int i);
+
+Vertex vertex_leaf(const int i, const int k);
+
+void edge_mark_tmp(const char *s, const Vertex v1, const Vertex v2, char *mark);
+
+int vertex_n_siblings(const Vertex e);
+
+#endif
---
+++ src/vertex.c
@@ -0,0 +1,38 @@
+
+#include "vertex.h"
+
+Vertex vertex(const int i)
+{
+    return vertex_leaf(i, -1);
+}
+
+Vertex vertex_leaf(const int i, const int k)
+{
+    Vertex v;
+    v = malloc(sizeof(struct vertex));
+    v->i = i;
+    v->k = k;
+    v->c = NULL;
+    v->s = NULL;
+    v->p = NULL;
+
+    return v;
+}
+
+
+void edge_mark_tmp(const char *s, const Vertex v1, const Vertex v2, char *mark)
+{
+    sstring(mark, v1->i, v2->i - v1->i, s);
+}
+
+
+int vertex_n_siblings(const Vertex e)
+{
+    int n = 0;
+    Vertex scan = e->c;
+    while (scan) {
+        scan = scan->s;
+        n++;
+    }
+    return n;
+}
