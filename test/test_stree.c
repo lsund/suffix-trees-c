3140958565a1dc5348c597cdee449e6bb0207b53
Ludvig Sundström <lud.sund@gmail.com>
2018-01-31 13:32:43 +0100

    Further testing

--- test/test_stree.c
+++ test/test_stree.c
@@ -106,28 +106,38 @@ char *utest_scan_prefix2()
     vertex_extend_right(v2, v5);        // ba -> abab
     vertex_extend_right(v7, v8);        // abab -> b
 
-    /* TreeMatching2 tm1 = scan_prefix2(r, label2(0, 1)); */
-    /* TreeMatching2 tm2 = scan_prefix2(r, label2(1, 2)); */
-    /* TreeMatching2 tm3 = scan_prefix2(r, label2(0, 2)); */
+    TreeMatching2 tm1 = scan_prefix2(r, label2(0, 1));
+    TreeMatching2 tm2 = scan_prefix2(r, label2(1, 2));
+    TreeMatching2 tm3 = scan_prefix2(r, label2(0, 2));
     TreeMatching2 tm4 = scan_prefix2(r, label2(2, 6));          // aaba
+    TreeMatching2 tm5 = scan_prefix2(r, label2(0, 5));          // abaab
+    TreeMatching2 tm6 = scan_prefix2(r, label2(4, 7));          // bab
 
-    /* mu_assert("End should exist #1", tm1.end); */
-    /* mu_assert("End should exist #2", tm2.end); */
-    /* mu_assert("End should exist #3", tm3.end); */
+    mu_assert("End should exist #1", tm1.end);
+    mu_assert("End should exist #2", tm2.end);
+    mu_assert("End should exist #3", tm3.end);
     mu_assert("End should exist #4", tm4.end);
+    mu_assert("End should exist #4", tm5.end);
+    mu_assert("End should exist #4", tm6.end);
 
-    /* tmp[0] = 0; */
-    /* vertex_mark(tmp, tm1.end, text); */
-    /* mu_assert("control of label #1", strcmp(tmp, "a") == 0); */
-    /* tmp[0] = 0; */
-    /* vertex_mark(tmp, tm2.end, text); */
-    /* mu_assert("control of label #2", strcmp(tmp, "ba") == 0); */
-    /* tmp[0] = 0; */
-    /* vertex_mark(tmp, tm3.end, text); */
-    /* mu_assert("control of label #3", strcmp(tmp, "ba") == 0); */
+    tmp[0] = 0;
+    vertex_mark(tmp, tm1.end, text);
+    mu_assert("control of label #1", strcmp(tmp, "a") == 0);
+    tmp[0] = 0;
+    vertex_mark(tmp, tm2.end, text);
+    mu_assert("control of label #2", strcmp(tmp, "ba") == 0);
+    tmp[0] = 0;
+    vertex_mark(tmp, tm3.end, text);
+    mu_assert("control of label #3", strcmp(tmp, "ba") == 0);
     tmp[0] = 0;
     vertex_mark(tmp, tm4.end, text);
     mu_assert("control of label #3", strcmp(tmp, "abab") == 0);
+    tmp[0] = 0;
+    vertex_mark(tmp, tm5.end, text);
+    mu_assert("control of label #3", strcmp(tmp, "abab") == 0);
+    tmp[0] = 0;
+    vertex_mark(tmp, tm6.end, text);
+    mu_assert("control of label #3", strcmp(tmp, "b") == 0);
 
     return NULL;
 }
